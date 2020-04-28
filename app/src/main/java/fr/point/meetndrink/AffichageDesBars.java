70ae4331aceb6efa6e41ba73321c28a79177ea80
Noenan <57996737+Noenan@users.noreply.github.com>
2020-04-28 21:37:19 +0200

    feat/AffichageBar
    branche fonctionnelle

--- app/src/main/java/fr/point/meetndrink/AffichageDesBars.java
+++ app/src/main/java/fr/point/meetndrink/AffichageDesBars.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
@@ -19,23 +22,31 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import static android.content.Intent.*;
+
 public class AffichageDesBars extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference barRef = db.collection("bars");
     private BarAdapter adapter;
     private String TAG;
+    Button mBtnShare;
+    Intent mShareIntent;
+    String mShareBody = "VOICI UNE APPLICATION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_affichage_des_bars);
 
+
+
+
         setUpRecyclerView();
     }
 
 
     private void setUpRecyclerView() {
-        Query query = barRef.orderBy("name", Query.Direction.ASCENDING);
+        Query query = barRef.orderBy("classification", Query.Direction.DESCENDING);
 
         FirestoreRecyclerOptions<Bar> options = new FirestoreRecyclerOptions.Builder<Bar>()
                 .setQuery(query,Bar.class)
@@ -43,20 +54,20 @@ public class AffichageDesBars extends AppCompatActivity {
 
         adapter = new BarAdapter(options);
 
-//        db.collection("bars")
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()){
-//                            for (QueryDocumentSnapshot document : task.getResult()){
-//                                Log.d(TAG, document.getId() + "=>"+ document.getData());
-//                            }
-//                        }else{
-//                            Log.w(TAG, "erreur bdd ", task.getException());
-//                        }
-//                    }
-//                });
+        db.collection("bars")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + "=>"+ document.getData());
+                            }
+                        }else{
+                            Log.w(TAG, "erreur bdd ", task.getException());
+                        }
+                    }
+                });
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
