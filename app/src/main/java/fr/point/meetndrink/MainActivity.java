6fb5349691b5b97a4123293d68e35b735e36b77d
Vinualez <57996003+Vinualez@users.noreply.github.com>
2020-04-30 09:59:02 +0200

    Bouton reservation

--- app/src/main/java/fr/point/meetndrink/MainActivity.java
+++ app/src/main/java/fr/point/meetndrink/MainActivity.java
@@ -1,5 +1,7 @@
 package fr.point.meetndrink;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,8 +9,9 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView bars, profil, amis, parametres;
+    ImageView bars, profil, amis, parametres, reser;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +20,22 @@ public class MainActivity extends AppCompatActivity {
         profil = findViewById(R.id.imageview_profil);
         amis = findViewById(R.id.imageview_friends);
         parametres = findViewById(R.id.imageview_settings);
+        reser = findViewById(R.id.reser);
 
         //Action du clic sur le bouton bar à écrire
         bars.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+            }
+        });
+
+        //Action du clic sur le bouton  à écrire
+        reser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Reservation.class);
+                startActivity(i);
 
             }
         });
